cd637e5fe4e9c51fd890548f446db72a55d09c18
CeMaGo <cecilemarleen@gmail.com>
2025-02-27 10:25:58 +0100

    HashMap solution

--- 876/383/383.js
+++ 876/383/383.js
@@ -1,18 +1,39 @@
+// /**
+//  * @param {string} ransomNote
+//  * @param {string} magazine
+//  * @return {boolean}
+//  */
+// var canConstruct = function(ransomNote, magazine) {
+//     for ( let i = 0; i < ransomNote.length; i++) {
+//         let r = ransomNote[i];
+//         let matchingIndex = magazine.indexOf(r);
+
+//         if (matchingIndex === -1) {
+//             return false;
+//         }
+
+//         magazine = magazine.slice(0, matchingIndex) + magazine.slice(matchingIndex + 1)
+//     }
+//     return true;
+// };
+
 /**
  * @param {string} ransomNote
  * @param {string} magazine
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    for ( let i = 0; i < ransomNote.length; i++) {
-        let r = ransomNote[i];
-        let matchingIndex = magazine.indexOf(r);
+    let charCount = {};
 
-        if (matchingIndex === -1) {
+    for (let char of magazine ){
+        charCount[char] = (charCount[char] || 0) +1;
+    }
+
+    for (let char of ransomNote) {
+        if (!charCount[char] || charCount[char] === 0) {
             return false;
         }
-
-        magazine = magazine.slice(0, matchingIndex) + magazine.slice(matchingIndex + 1)
+        charCount[char]--;
     }
     return true;
-};
+}
