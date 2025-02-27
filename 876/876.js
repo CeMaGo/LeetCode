b3fe876a0032e11f8a884699b09b81545f64e38c
CeMaGo <cecilemarleen@gmail.com>
2025-02-27 08:07:31 +0100

    876 JS

--- 876/876.js
+++ 876/876.js
@@ -1,21 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var middleNode = function(head) {
-    let slow = head;
-    let fast = head;
-    
-    while (fast !== null && fast.next !== null) {
+  let slow = head; // this does not create new copies of the linked list. Instead, both slow and fast
+  let fast = head; // are just references (pointers) to the same starting node.
+
+  while ( fast !== null && fast.next !== null ) {
       slow = slow.next;
       fast = fast.next.next;
-    }
-    return slow;
   }
-  
-  function createLinkedList(arr) {
-    let dummy = new ListNode(0);
-    let curr = dummy;
-    
-    for ( let val of arr) {
-      curr.next = new ListNode(val);
-      curr = curr.next;
-    }
-    return dummy.next;
-  }
+  return slow;
+}
+
+// Time Complexity = O(n);
+// Space Complexity = O(1);
