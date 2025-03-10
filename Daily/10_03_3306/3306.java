6fbd9dcaa6d5124d33b8ca87a878f506f23f9a74
CeMaGo <cecilemarleen@gmail.com>
2025-03-10 17:02:17 +0100

    still not there

--- Daily/10_03_3306/3306.java
+++ Daily/10_03_3306/3306.java
@@ -1,37 +1,50 @@
+import java.util.List;
+
 class Solution {
     public int countOfSubstrings(String word, int k) {
         int n = word.length();
-        int count = 0;
-
+        List<Integer> vowelPositions = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                String substring = word.substring(i, j + 1);
-                if (hasAllVowels(substring) && countConsonants(substring) == k) {
-                    count++;
+            if (isVowel(word.charAt(i))) {
+                vowelPositions.add(i);
+            }
+        }
+
+        int count = 0;
+        if (vowelPositions.size() < 5) return 0;
+
+        for (int a = 0; a < vowelPositions.size(); a++) {
+            for (int e = 0; e < vowelPositions.size(); e++){
+                for (int i = 0; i < vowelPositions.size(); i++) {
+                    for (int o = 0; o < vowelPositions.size(); o++) {
+                        for (int u = 0; u < vowelPositions.size(); u++) {
+                            int[] indices = {vowelPositions.get(a),
+                                             vowelPositions.get(e),
+                                             vowelPositions.get(i),
+                                             vowelPositions.get(o),
+                                             vowelPositions.get(u)
+                                            };
+                                            java.util.Arrays.sort(indices);
+                                            int left = indices[0];
+                                            int right = indices[4];
+
+                                            int consonantCount = 0;
+                                            for (int idx = left; idx <= right; idx++) {
+                                                if ( !isVowel(word.charAt(idx))) {
+                                                    consonantCount++;
+                                                }
+                                            }
+                                            if ( consonantCount == k) {
+                                                count++;
+                                            }
+                        }
+                    }
                 }
             }
+            return count;
         }
-        return count;
-    }
-
-    private boolean hasAllVowels(String str) {
-        String vowels = "aeiou";
-        for (char vowel : vowels.toCharArray()) {
-            if (str.indexOf(vowel) == -1) {
-                return false;
-            }
+        private boolean isVowel(char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c = 'o' || c = 'u';
         }
-        return true;
-    }
-
-    private int countConsonants(String str) {
-        String vowels = "aeiou";
-        int count = 0;
-        for (char c : str.toCharArray()) {
-            if (vowels.indexOf(c) == -1 && Character.isLetter(c)) {
-                count++;
-            }
-        }
-        return count;
     }
 }
